395cb881f6bac7e719e389f685c64e964ddd84b4
fedor-resh <87930362+fedor-resh@users.noreply.github.com>
2022-05-14 12:16:29 +0300

    add class BoughtBooks

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import BookStore from './BookStore';
 import {RecoilRoot} from "recoil";
+import './assets/fonts/fonts.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
